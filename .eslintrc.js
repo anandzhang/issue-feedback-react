19051c9d2324dd7853ae66bc451bddc337567743
anandzhang <anandzhangnew@gmail.com>
2020-05-12 11:09:01 +0800

    添加 ESlint

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 11,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  ignorePatterns: [
+    'src/serviceWorker.js'
+  ],
+  rules: {
+    'no-console': 'error',
+    'no-alert': 'error'
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from 'react'
 
-function App() {
+function App () {
   return (
     <div>Hello</div>
-  );
+  )
 }
 
-export default App;
+export default App
