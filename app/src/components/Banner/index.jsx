a6fb75e76d18028e61e6f6b928e7f4a9a8cfc404
anandzhang <anandzhangnew@gmail.com>
2020-05-13 21:19:05 +0800

    修改 Banner 为函数组件

--- app/src/components/Banner/index.jsx
+++ app/src/components/Banner/index.jsx
@@ -1,15 +1,13 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './index.css'
 
-class Banner extends Component {
-  render () {
-    return (
-      <div className='banner'>
-        <p className='text-large'>你的反馈</p>
-        <p className='text-middle'>促使更好的产品</p>
-      </div>
-    )
-  }
+function Banner () {
+  return (
+    <div className='banner'>
+      <p className='text-large'>你的反馈</p>
+      <p className='text-middle'>促使更好的产品</p>
+    </div>
+  )
 }
 
 export default Banner
