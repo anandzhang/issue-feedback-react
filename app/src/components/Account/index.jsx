a837d94b0ee985aea724669f7af91fbea548e734
anandzhang <anandzhangnew@gmail.com>
2020-05-14 22:48:14 +0800

    修改登录/注册Modal

--- app/src/components/Account/index.jsx
+++ app/src/components/Account/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { Modal, Form, Input, message } from 'antd'
+import { Modal, Form, Input, message, Button } from 'antd'
+import './index.css'
 
 const { Item } = Form
 
@@ -35,21 +36,26 @@ class Login extends Component {
     } catch (err) { }
   }
 
+  changeMode = () => {
+    this.setState({
+      login: false
+    })
+  }
+
   render () {
     const { login, visible } = this.state
     return (
       <Modal
         title={login ? '登录' : '注册'}
         visible={visible}
-        onOk={login ? this.handleLogin : this.handleRegister}
         onCancel={this.changeVisible}
-        okText='确认'
-        cancelText='取消'
+        footer={null}
       >
         <Form
           ref={this.form}
           labelCol={{ span: 2, offset: 3 }}
           wrapperCol={{ span: 14, offset: 1 }}
+          onFinish={login ? this.handleLogin : this.handleRegister}
         >
           <Item
             label='邮箱'
@@ -64,6 +70,25 @@ class Login extends Component {
             rules={[{ required: true, message: '请输入密码' }]}
           >
             <Input.Password />
+          </Item>
+          <Item wrapperCol={{ span: 17, offset: 3 }}>
+            <Button
+              type='primary'
+              htmlType='submit'
+              className='form-submit-btn'
+            >
+              {login ? '登录' : '注册'}
+            </Button>
+          </Item>
+          <Item>
+            没有账号？去
+            <Button
+              type='link'
+              onClick={this.changeMode}
+              className='no-padding'
+            >
+              注册
+            </Button>
           </Item>
         </Form>
       </Modal>
