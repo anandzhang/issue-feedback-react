89adc29ff4f420e5f5ac3f7e3906e361c83b7afe
anandzhang <anandzhangnew@gmail.com>
2020-05-20 17:48:24 +0800

    添加一些常用参数

--- startup.sh
+++ startup.sh
@@ -1,5 +1,40 @@
 #!/bin/bash
 
-cd app/ && npm install
-docker-compose -f ../docker/docker-compose.yml up -d
-echo 'app is running on http://localhost/'
+backendUrl='localhost:8923'
+dockerCmd='docker-compose -f ../docker/docker-compose.yml'
+options=('restart 重启服务'
+  'web 进入Nginx容器'
+  'app 进入项目容器'
+  'kill 停止并删除容器')
+
+in_cyan() { echo -e "\e[1;36m$*\e[0m"; }
+
+if [ $(curl -sI -w "%{http_code}" -o /dev/null ${backendUrl}) != '404' ]; then
+  in_cyan "请先启动后端服务"
+else
+  if [ $# == 0 ]; then
+    ${dockerCmd} up -d
+    in_cyan "\napp is running on http://localhost/"
+  else
+    case $1 in
+    'restart')
+      ${dockerCmd} restart
+      ;;
+    'web')
+      ${dockerCmd} up -d
+      ;;
+    'app')
+      ${dockerCmd} exec issue-feedback-react bash
+      ;;
+    'kill')
+      ${dockerCmd} down
+      ;;
+    *)
+      in_cyan 这是帮助信息，脚本提供如下参数：
+      for index in ${!options[@]}; do
+        in_cyan "\n$(expr $index + 1). ${options[$index]}"
+      done
+      ;;
+    esac
+  fi
+fi
