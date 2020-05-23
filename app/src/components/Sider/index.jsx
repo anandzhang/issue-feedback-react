1f97f8fb14720545dcd63434b53776d82c5e5349
anandzhang <anandzhangnew@gmail.com>
2020-05-23 22:26:14 +0800

    路由切换和加载时默认选中

--- app/src/components/Sider/index.jsx
+++ app/src/components/Sider/index.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Link, withRouter } from 'react-router-dom'
 import { Menu } from 'antd'
 import menuConfig from './menuConfig'
 import { SubMenu } from 'rc-menu'
@@ -6,41 +8,67 @@ import { SubMenu } from 'rc-menu'
 const { Item } = Menu
 
 class Sider extends Component {
+  constructor (props) {
+    super(props)
+    this.menu = this.getMenu()
+  }
+
+  getMenu = () => {
+    return menuConfig.map(value => {
+      const { children } = value
+      if (children) {
+        const { title, icon, route } = value
+        return (
+          <SubMenu
+            key={route}
+            title={
+              <span>
+                {icon}<span>{title}</span>
+              </span>
+            }
+          >
+            {children.map(value => this.getMenuItem(value))}
+          </SubMenu>
+        )
+      }
+      return this.getMenuItem(value)
+    })
+  }
+
+  getMenuItem = value => {
+    const { title, icon, route } = value
+    return (
+      <Item key={route} icon={icon}>
+        <Link to={route}>{title}</Link>
+      </Item>
+    )
+  }
+
+  getParentPath = pathname => {
+    pathname = pathname.split('/')
+    pathname.pop()
+    return pathname.join('/')
+  }
+
   render () {
+    const { pathname } = this.props.location
+    const parentPath = this.getParentPath(pathname)
     return (
       <div>
-        <Menu mode='inline'>
-          {
-            menuConfig.map(value => {
-              const { title, icon, route, children } = value
-              if (children) {
-                return (
-                  <SubMenu
-                    key={route}
-                    title={
-                      <span>
-                        {icon}
-                        <span>{title}</span>
-                      </span>
-                    }
-                  >
-                    {
-                      children.map(value => (
-                        <Item key={value.route} icon={value.icon}>{value.title}</Item>
-                      ))
-                    }
-                  </SubMenu>
-                )
-              }
-              return (
-                <Item key={route} icon={icon}>{title}</Item>
-              )
-            })
-          }
+        <Menu
+          mode='inline'
+          defaultSelectedKeys={[pathname]}
+          defaultOpenKeys={[parentPath]}
+        >
+          {this.getMenu()}
         </Menu>
       </div>
     )
   }
 }
 
-export default Sider
+Sider.propTypes = {
+  location: PropTypes.object
+}
+
+export default withRouter(Sider)
