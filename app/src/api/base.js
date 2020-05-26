db9ac6a58fea5de67620860f28a107dcc1843472
anandzhang <anandzhangnew@gmail.com>
2020-05-26 16:00:56 +0800

    修改以前的baseApi

--- app/src/api/base.js
+++ app/src/api/base.js
@@ -1,28 +1,24 @@
-import axios from 'axios'
-import Storage from '../utils/Storage'
-import { ACCOUNT_URL, PROFILE_URL } from './config'
 import Api from './Api'
+import {
+  ACCOUNT,
+  PROFILE
+} from './config'
+import Storage from '../utils/Storage'
 
-axios.interceptors.response.use(response => response.data)
+// Account Request
+export const requestSendCode = data => Api.request(...ACCOUNT.SEND_CODE, data)
+export const requestRegister = data => Api.request(...ACCOUNT.REGISTER, data)
+export const requestLogin = data => Api.request(...ACCOUNT.LOGIN, data)
 
-export const requestSendCode = async data => {
-  return await axios.post(ACCOUNT_URL.SEND_CODE, data)
-}
-
-export const requestRegister = async data => {
-  return await axios.post(ACCOUNT_URL.REGISTER, data)
-}
-
-export const requestLogin = async data => {
-  return Api.request('/login', 'POST', data)
-}
-
-export const requsetProfile = async () => {
+// Profile Request
+export const requsetProfile = () => {
   const userId = Storage.get('userId')
-  return await axios.get(`${PROFILE_URL.GET}/${userId}`)
+  const config = ACCOUNT.GET
+  config.url = config.url.replace('<user_id>', userId)
+  return Api.request(...config)
 }
 
 export const requestUpdateProfile = async data => {
   const userId = Storage.get('userId')
-  return await axios.put(PROFILE_URL.UPDATE, { user_id: userId, ...data })
+  return Api.request(...PROFILE.UPDATE, { user_id: userId, ...data })
 }
