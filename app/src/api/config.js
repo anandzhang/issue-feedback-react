f76c11c07824616c847a8f788456453b10864f1c
anandzhang <anandzhangnew@gmail.com>
2020-05-26 19:28:21 +0800

    跟随后端修改api

--- app/src/api/config.js
+++ app/src/api/config.js
@@ -13,14 +13,14 @@ export const PROFILE = {
   UPDATE: config('/profile/<user_id>', 'PUT')
 }
 
-export const FEEDBACK = {
-  CREATE: config('/issue', 'POST'),
-  VOTE: config('/issue/<issue_id>/user/<user_id>/vote', 'PUT'),
-  LIST: config('/issue/product/<product_id>/opening')
-}
-
 export const PRODUCT = {
   CREATE: config('/product', 'POST'),
   LIST: config('/products'),
   LIST_BY_MANAGER: config('/product/manager/<manager_id>')
+}
+
+export const FEEDBACK = {
+  CREATE: config('/issue', 'POST'),
+  VOTE: config('/issue/<issue_id>/vote', 'PUT'),
+  LIST: config('/issue/product/<product_id>')
 }
