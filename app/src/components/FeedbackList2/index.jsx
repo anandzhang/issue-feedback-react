461f784add0f3c8c3794bb8273a7efdf4364b7d2
anandzhang <anandzhangnew@gmail.com>
2020-05-27 21:02:18 +0800

    添加投票功能

--- app/src/components/FeedbackList2/index.jsx
+++ app/src/components/FeedbackList2/index.jsx
@@ -1,22 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { List, Avatar, Space } from 'antd'
-import { LikeOutlined, DislikeOutlined } from '@ant-design/icons'
+import { List, Avatar } from 'antd'
 import moment from 'moment'
 import 'moment/locale/zh-cn'
+import getItemActions from './getItemActions'
 import './index.css'
 
 const { Item } = List
 const { Meta } = Item
 
 const FeedbackList2 = ({ dataSource }) => {
-  const actions = [
-    <Space key='like'><LikeOutlined />200</Space>,
-    <Space key='dislike'><DislikeOutlined />10</Space>
-  ]
-
   const renderItem = item => {
-    const { title, description, created_at: time } = item
+    const { issue_id: id, title, description, created_at: time } = item
     const avatar = (
       <Avatar
         size='large'
@@ -27,7 +22,7 @@ const FeedbackList2 = ({ dataSource }) => {
       />
     )
     return (
-      <Item actions={actions} className='feedback-list-item'>
+      <Item actions={getItemActions(id)} className='feedback-list-item'>
         <Meta
           avatar={avatar}
           title={title}
