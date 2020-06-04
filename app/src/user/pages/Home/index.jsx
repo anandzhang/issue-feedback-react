d84449ccfbee499abdf0e71dec37748b3eab96b7
anandzhang <anandzhangnew@gmail.com>
2020-06-04 18:24:58 +0800

    修改 FeedbackList

--- app/src/user/pages/Home/index.jsx
+++ app/src/user/pages/Home/index.jsx
@@ -5,7 +5,6 @@ import { saveProducts, saveFeedback } from '../../../actions'
 import { Row, Col, Button, Card, message } from 'antd'
 import Banner from '../../components/Banner'
 import FeedbackList from '../../components/FeedbackList'
-import FixedFeedbackList from '../../components/FixedFeedbackList'
 import UserStatistic from '../../components/UserStatistic'
 import AddModal from './AddModal'
 import { requestFeedbackList, requestProductList } from '../../../api/base'
@@ -68,14 +67,16 @@ const Home = props => {
       <div className='feedback-title'>最近反馈</div>
       <Row gutter={12}>
         <Col span={18}>
-          <FeedbackList dataSource={feedback.opening} />
+          <FeedbackList status='opening' dataSource={feedback.opening} />
         </Col>
         <Col span={6}>
           <Card className='margin-10'>
             <UserStatistic like={10} feedback={20} />
             <Button type='primary' className='feedback-btn' onClick={showAddModal}>反馈一下</Button>
           </Card>
-          <FixedFeedbackList dataSource={feedback.closed} />
+          <Card title='最新动态'>
+            <FeedbackList status='closed' dataSource={feedback.closed} />
+          </Card>
         </Col>
       </Row>
       <AddModal ref={addModal} getFeedback={getFeedback} />
