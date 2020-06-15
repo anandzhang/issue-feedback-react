eb0821075ff9032413afb272c652a110564f342a
anandzhang <anandzhangnew@gmail.com>
2020-06-15 21:16:57 +0800

    添加对应的路由

---
+++ app/src/admin/pages/Problem/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Card } from 'antd'
+
+const Problem = () => {
+  return (
+    <Card title='常见问题'>
+
+    </Card>
+  )
+}
+
+export default Problem
--- app/src/routes/admin.js
+++ app/src/routes/admin.js
@@ -3,6 +3,7 @@ import Home from '../admin/pages/Home'
 import Feedback from '../admin/pages/Feedback'
 import FeedbackDetail from '../admin/pages/FeedbackDetail'
 import Product from '../admin/pages/Product'
+import Problem from '../admin/pages/Problem'
 import Role from '../admin/pages/Role'
 
 export default [
@@ -19,7 +20,7 @@ export default [
     children: <Feedback />
   },
   {
-    // 反馈管理
+    // 反馈管理中的反馈详情
     path: '/admin/manage/feedback/detail',
     exact: false,
     children: <FeedbackDetail />
@@ -29,6 +30,12 @@ export default [
     path: '/admin/manage/product',
     exact: false,
     children: <Product />
+  },
+  {
+    // 常见问题设置
+    path: '/admin/setting/problem',
+    exact: false,
+    children: <Problem />
   },
   {
     // 后台成员
