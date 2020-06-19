3f529dc09ce6e763120444f35b7613868360bd7a
anandzhang <anandzhangnew@gmail.com>
2020-06-19 22:21:17 +0800

    修改主页已关闭反馈显示的链接

--- app/src/comon/SimpleList/index.jsx
+++ app/src/comon/SimpleList/index.jsx
@@ -1,21 +1,37 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 import { List } from 'antd'
 
 const { Item } = List
 
-const SimpleList = ({ dataSource, itemName }) => {
+const SimpleList = ({ dataSource, itemName, linkIndex }) => {
+  const renderItem = item => {
+    return (
+      <Item>
+        {linkIndex
+          ? (
+            <Link to={`/feedback/${item[linkIndex]}`}>
+              {item[itemName]}
+            </Link>
+          )
+          : item[itemName]}
+      </Item>
+    )
+  }
+
   return (
     <List
       dataSource={dataSource}
-      renderItem={item => <Item>{item[itemName]}</Item>}
+      renderItem={renderItem}
     />
   )
 }
 
 SimpleList.propTypes = {
   dataSource: PropTypes.array.isRequired,
-  itemName: PropTypes.string.isRequired
+  itemName: PropTypes.string.isRequired,
+  linkIndex: PropTypes.string
 }
 
 export default SimpleList
