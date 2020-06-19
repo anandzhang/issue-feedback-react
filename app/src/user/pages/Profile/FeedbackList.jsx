84cbcbd484380798d2fe5b270b864e102e53ce0f
anandzhang <anandzhangnew@gmail.com>
2020-06-19 22:23:53 +0800

    添加个人中心上的反馈链接

--- app/src/user/pages/Profile/FeedbackList.jsx
+++ app/src/user/pages/Profile/FeedbackList.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { List } from 'antd'
 import chinaDate from '../../../utils/chinaDate'
+import { Link } from 'react-router-dom'
 
 const { Item } = List
 
@@ -13,7 +14,9 @@ const FeedbackList = props => {
       renderItem={item => (
         <Item>
           <Item.Meta
-            title={item.title}
+            title={(
+              <Link to={`/feedback/${item.issue_id}`}>{item.title}</Link>
+            )}
             description={chinaDate(item.created_at).format('lll')}
           />
         </Item>
