e388bcd35a7f2a9cde6ae441e7cdb178dee25084
anandzhang <anandzhangnew@gmail.com>
2020-06-19 21:57:31 +0800

    修复主页 Menu 选中问题

--- app/src/user/components/Header/LeftPart.jsx
+++ app/src/user/components/Header/LeftPart.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { Col, Menu } from 'antd'
 import navMenuConfig from './navMenuConfig'
 
@@ -16,13 +16,22 @@ const getMenuItem = () => navMenuConfig.map(value => {
 
 const menu = getMenuItem()
 
-const LeftPart = () => (
-  <Col span={6}>
-    <Menu mode='horizontal' style={stylesheet.menu}>
-      {menu}
-    </Menu>
-  </Col>
-)
+const LeftPart = () => {
+  const location = useLocation()
+  const { pathname } = location
+
+  return (
+    <Col span={6}>
+      <Menu
+        mode='horizontal'
+        defaultSelectedKeys={[pathname]}
+        style={stylesheet.menu}
+      >
+        {menu}
+      </Menu>
+    </Col>
+  )
+}
 
 const stylesheet = {
   menu: {
