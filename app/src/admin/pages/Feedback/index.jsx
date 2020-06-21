09273f73989028e63acf2611fefae8ac7d69ce07
anandzhang <anandzhangnew@gmail.com>
2020-06-21 13:56:38 +0800

    修复反馈管理修改反馈状态时的渲染问题

--- app/src/admin/pages/Feedback/index.jsx
+++ app/src/admin/pages/Feedback/index.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { useHistory } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { saveFeedback } from '../../../actions'
 import { Card, Table, Button, message } from 'antd'
 import CardTitle from './CardTitle'
 import columns from './columns'
@@ -9,21 +10,27 @@ import STATUS from '../../../constants/Status'
 import { requestUpdateFeedbackStatus } from '../../../api/base'
 
 const Feedback = props => {
-  const { feedback } = props
+  const { feedback, saveFeedback } = props
   const [status, setStatus] = useState(STATUS.OPENING)
   const history = useHistory()
 
   const modifyFeedbackStatus = async (id, status) => {
+    let newStatus = ''
     let msg = ''
     if (status === STATUS.OPENING) {
-      status = STATUS.CLOSED
+      newStatus = STATUS.CLOSED
       msg = '关闭'
     } else {
-      status = STATUS.OPENING
+      newStatus = STATUS.OPENING
       msg = '开启'
     }
     try {
-      await requestUpdateFeedbackStatus(id, status)
+      await requestUpdateFeedbackStatus(id, newStatus)
+      // 修改状态库中数据 避免再次请求反馈列表
+      const newFeedback = [...feedback[status]]
+      const index = newFeedback.findIndex(item => item.issue_id === id)
+      newFeedback.splice(index, 1)
+      saveFeedback({ status, data: newFeedback })
       message.success(`${msg}成功`)
     } catch {
       message.error(`${msg}失败`)
@@ -73,7 +80,11 @@ const Feedback = props => {
 }
 
 Feedback.propTypes = {
-  feedback: PropTypes.object
+  feedback: PropTypes.object,
+  saveFeedback: PropTypes.func
 }
 
-export default connect(({ feedback }) => ({ feedback }))(Feedback)
+export default connect(
+  ({ feedback }) => ({ feedback }),
+  { saveFeedback }
+)(Feedback)
