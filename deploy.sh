45053f17aaafbdc6666f465830436cb41e3e475c
anandzhang <anandzhangnew@gmail.com>
2020-06-21 17:23:10 +0800

    完整的clone

--- deploy.sh
+++ deploy.sh
@@ -3,36 +3,37 @@
 # Author: Anand Zhang
 
 # 常量
-readonly BACKEND_PROJECT_NAME='issue_feedback_sanic'
+readonly OUTPUTDIR='/usr/share/feedback_system'
+readonly FRONTEND_PROJECT_URL='https://github.com/anandzhang/issue-feedback-react.git'
 readonly BACKEND_PROJECT_URL='https://github.com/wangsiqian/issue_feedback_sanic.git'
 
 get_backend_server() {
-  if [ ! -d $OUTPUTDIR ]; then
-    mkdir $OUTPUTDIR
-  fi
-  cd ..
-  git clone $BACKEND_PROJECT_URL
-  cd -
+  cd $OUTPUTDIR
+  git clone $BACKEND_PROJECT_URL $OUTPUTDIR/backend
   # 启动后端服务
-  cd ../$BACKEND_PROJECT_NAME/docker
+  cd backend/docker
   docker-compose exec -d issue_feedback_sanic bash run_server.sh
-  cd -
 }
 
 build_frontend_project() {
-  cd app
+  cd $OUTPUTDIR
+  git clone $FRONTEND_PROJECT_URL $OUTPUTDIR/frontend
+  cd frontend/app
+  npm install --production
   npx react-app-rewired build
-  cd -
 }
 
 main() {
+  if [ ! -d $OUTPUTDIR ]; then
+    mkdir $OUTPUTDIR
+  fi
   echo -e '\n---------- 1. 获取后端服务 ----------\n'
   get_backend_server
   echo -e '\n---------- 2. 前端项目 build 构建 ----------\n'
   build_frontend_project
   echo -e '\n---------- 3. 使用 Nginx 容器托管静态文件 ----------\n'
   docker run --name feedback_web_system \
-    -p 180:80 \
+    -p 80:80 \
     -v $(pwd)/app/build:/opt/app \
     -v $(pwd)/docker/deploy.conf:/etc/nginx/conf.d/default.conf \
     --link issue_feedback_sanic:backend \
